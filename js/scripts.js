99f6ae50efe588aea491d8454f83ecf7663407c8
Niem Nguyen <luuniem87@gmail.com>
2017-03-08 00:13:00 -0800

    change slider

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,26 @@ $(function(){
       selectYears: 15 // Creates a dropdown of 15 years to control year
     });
 });
+
+var hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener('click', function(){
+hamburger.classList.toggle('is-active');
+});
+
+var topSlider = new Swiper ('.swiper-container', {
+  effect: 'coverflow',
+  coverflow: {
+  rotate: 50,
+  stretch: 0,
+  depth: 100,
+  modifier: 1,
+  slideShadows : true
+},
+  pagination: '.swiper-pagination'
+});
+
+var album1 = new Swiper('.swiper-album1', {
+  effect: 'flip',
+  pagination: '.swiper-pagination'
+});
