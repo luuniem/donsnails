2830999e4ba2f83d4fb02dea47d566f51cc4de98
Niem Nguyen <luuniem87@gmail.com>
2017-03-07 21:47:42 -0800

    add non functional form

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,8 @@ $(function(){
      indicators: true
    });
    $('.modal').modal();
+   $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
 });
