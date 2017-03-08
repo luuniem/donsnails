7292c45e581dff820fa3aac3b915aa415fa27500
Niem Nguyen <luuniem87@gmail.com>
2017-03-07 21:02:01 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".button-collapse").sideNav();
+   $('.carousel.carousel-slider').carousel({fullWidth: true});
+   $('.carousel').carousel({
+     indicators: true
+   });
+   $('.modal').modal();
+});
